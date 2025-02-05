e116b93a35149838761dfc198f9acfb42719d7b6
Ganesh-M01 <124779430+Ganesh-M01@users.noreply.github.com>
2025-02-05 11:46:09 +0530

    Name and Roles Updated

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,37 @@
 
 import './App.css'
-
+import { Typewriter } from 'react-simple-typewriter'
+import Navbar from "../components/Nav"
+ 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Hi Im ganesh</h1>
+        <Navbar/>
+
+      <div className='App'>
+      <h1>Ganesh</h1>
+      <h2 style={{ paddingTop: '0.5rem', margin: 'auto 0', fontWeight: 'normal' }}>
+        <span style={{ color: '#ffff', fontWeight: 'bold' }}>
+          
+          {/* Style will be inherited from the parent element */}
+          <Typewriter
+            words={['Fullstack Developer', 'Data Analyst', 'Devops Engineer']}
+            loop={4}
+            cursor
+            cursorStyle=''
+            typeSpeed={100}
+            deleteSpeed={100}
+            delaySpeed={2000}
+
+          />
+        </span>
+      </h2>
+    </div>
+
+
+        
         </div>
     </>
   )
