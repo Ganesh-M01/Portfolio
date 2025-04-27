47f4f6da622e31016881d748a78502202aa63ac7
Ganesh-M01 <124779430+Ganesh-M01@users.noreply.github.com>
2025-04-27 11:24:56 +0530

    Single_Page_Components updated

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,10 @@
-import './App.css'
-import { Typewriter } from 'react-simple-typewriter'
-import Navbar from "../components/Nav"
-import FollowCursor from '../components/FlowCursor'
-import { Routes, Route } from 'react-router-dom'
-import Home from './home'
-import Projects from './projects'
-import About from './about'
-import Contact from './contact'
+import './App.css';
+import Navbar from '../components/Nav';
+import FollowCursor from '../components/FlowCursor';
+import Home from './home';
+import About from './about';
+import Projects from './projects';
+import Contact from './contact';
 
 function App() {
   return (
@@ -14,15 +12,21 @@ function App() {
       <div>
         <FollowCursor />
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <div id="home">
+          <Home />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="contact">
+          <Contact />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
